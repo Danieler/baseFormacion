e1b0d02b574a6e98c1ee44f78aa54c50fa3a1c5a
Dani <daniel.esteban87@gmail.com>
2019-01-29 08:58:22 +0100

    add map

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import sayHello from './hello';
 import './index.scss';
+import Map from './map';
 
+
+let map = new Map();
+map.createMarkers();
 document.getElementById('root').innerHTML = sayHello();
 
 
